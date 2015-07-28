f974024cb6cf37b688f1c7e7c220becfe705ae2f
Alex <shaposhnikovaa@ukr.net>
2015-07-28 10:50:37 +0300

    Completed 8 task

--- app/code/local/DS/News/controllers/Adminhtml/NewsController.php
+++ app/code/local/DS/News/controllers/Adminhtml/NewsController.php
@@ -5,27 +5,12 @@ class DS_News_Adminhtml_NewsController extends Mage_Adminhtml_Controller_Action
 
     public function indexAction()
     {
-        echo '<h1>News Module: Admin section</h1>';
-    }
+        $this->loadLayout();
+        $this->_setActiveMenu('dsnews');
 
-
-    public function massDeleteAction()
-    {
-        $news = $this->getRequest()->getParam('news', null);
-
-        if (is_array($news) && sizeof($news) > 0) {
-            try {
-                foreach ($news as $id) {
-                    Mage::getModel('dsnews/news')->setId($id)->delete();
-                }
-                $this->_getSession()->addSuccess($this->__('Total of %d news have been deleted', sizeof($news)));
-            } catch (Exception $e) {
-                $this->_getSession()->addError($e->getMessage());
-            }
-        } else {
-            $this->_getSession()->addError($this->__('Please select news'));
-        }
-        $this->_redirect('*/*');
+        $contentBlock = $this->getLayout()->createBlock('dsnews/adminhtml_news');
+        $this->_addContent($contentBlock);
+        $this->renderLayout();
     }
 
 }
