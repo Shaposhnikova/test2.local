86535c4febbc82565188407030a704efb87ab7ca
Alex <shaposhnikovaa@ukr.net>
2015-08-04 13:56:56 +0300

    Completed 8 task

---
+++ app/code/local/CC/CatalogCustomization/Model/Observer.php
@@ -0,0 +1,14 @@
+<?php
+class CC_CatalogCustomization_Model_Observer extends Varien_Event_Observer
+{
+    public function __construct()
+    {
+    }
+    public function saveCmsPageObserve($observer)
+    {
+        $event = $observer->getEvent();
+        $model = $event->getPage();
+        print_r($model->getData());
+        die('test');
+    }
+}
