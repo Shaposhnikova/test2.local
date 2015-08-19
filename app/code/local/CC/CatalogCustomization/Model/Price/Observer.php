56094174e712b6cc548e419c36f3034997591016
Alex <shaposhnikovaa@ukr.net>
2015-08-19 13:25:11 +0300

    Completed 8 task

--- app/code/local/CC/CatalogCustomization/Model/Price/Observer.php
+++ app/code/local/CC/CatalogCustomization/Model/Price/Observer.php
@@ -8,13 +8,13 @@ class CC_CatalogCustomization_Model_Price_Observer
      * @return  CC_Catalog_Model_Price_Observer
      */
     public function applyDiscountPercent($observer)
-        {
+    {
         $event = $observer->getEvent();
         $product = $event->getProduct();
         // process percentage discounts only for simple products
         if ($product->getSuperProduct() && $product->getSuperProduct()->isConfigurable()) {
         } else {
-            $percentDiscount = $product->getPercentDiscount();
+            $percentDiscount = $percentDiscount = 30;//$product->getPercentDiscount();
 
             if (is_numeric($percentDiscount)) {
                 $today = floor(time()/86400)*86400;
@@ -27,20 +27,23 @@ class CC_CatalogCustomization_Model_Price_Observer
                     $price = $product->getPrice();
                     $finalPriceNow = $product->getData('final_price');
 
-                    $specialPrice = $price - $price * $percentDiscount / 100;
+                    $specialPrice = $price + $price * $percentDiscount / 100;
 
                     // if special price is negative - negate the discount - this may be a mistake in data
                     if ($specialPrice < 0)
                         $specialPrice = $finalPriceNow;
 
-                    if ($specialPrice < $finalPriceNow)
+                   // if ($specialPrice < $finalPriceNow)
                         $product->setFinalPrice($specialPrice); // set the product final price
                 }
             }
         }
+
         return $this;
     }
 }
+
+
 
 
 
