053315419076a730e1a4647105e2009b0142b8f0
Alex <shaposhnikovaa@ukr.net>
2015-11-05 12:16:41 +0200

    Completed 8 task

--- app/code/local/CC2/Video/sql/cc2_video_setup/mysql4-install-1.0.0.php
+++ app/code/local/CC2/Video/sql/cc2_video_setup/mysql4-install-1.0.0.php
@@ -1,28 +1,13 @@
 <?php
-
-$installer = $this;
-$installer->startSetup();
-
-$setup = new Mage_Eav_Model_Entity_Setup('core_setup');
-
-$entityTypeId     = $setup->getEntityTypeId('product');
-$attributeSetId   = $setup->getDefaultAttributeSetId($entityTypeId);
-$attributeGroupId = $setup->getDefaultAttributeGroupId($entityTypeId, $attributeSetId);
-
-$setup->addAttribute('product', 'video', array(
+$this->startSetup();
+$this->addAttribute(Mage_Catalog_Model_Product::ENTITY, 'video', array(
+    'group'         => 'General',
     'input'         => 'text',
     'type'          => 'text',
     'label'         => 'Video',
-    'visible'       => True,
+    'backend'       => '',
+    'visible'       => true,
     'required'      => false,
-    'user_defined' => true,
     'visible_on_front' => true,
 ));
-
-
-
-$oAttribute = Mage::getSingleton('eav/config')->getAttribute('product', 'video');
-$oAttribute->setData('used_in_forms', array('adminhtml_product'));
-$oAttribute->save();
-
-$setup->endSetup();
+$this->endSetup();
--- install.php
+++ install.php
@@ -1,155 +1,60 @@
 <?php
-/**
- * Magento
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@magentocommerce.com so we can send you a copy immediately.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade Magento to newer
- * versions in the future. If you wish to customize Magento for your
- * needs please refer to http://www.magentocommerce.com for more information.
- *
- * @category   Mage
- * @package    Mage
- * @copyright  Copyright (c) 2008 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
- * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- */
+
+// методы взяты из класса Mage_Eav_Model_Entity_Setup в файле D:\apache\_ROOT_\ZEND\mage\empty\app\code\core\Mage\Eav\Model\Entity\Setup.php)
+
+$installer = $this;
+$installer->startSetup();
+
+$entityTypeId     = $installer->getEntityTypeId('catalog_category');
+$attributeSetId   = $installer->getDefaultAttributeSetId($entityTypeId);
+//$attributeGroupId = $installer->getDefaultAttributeGroupId($entityTypeId, $attributeSetId); //можно получить дефолтную группу
+$attributeGroupId = $installer->getAttributeGroupId($entityTypeId, $attributeSetId, 'General Information'); //получить группу по названию или ID
+
+$installer->addAttribute('catalog_category', 'unique_id',  array(
+    'type'     => 'text',
+    'label'    => 'Unique ID',
+    'input'    => 'text',
+    'global'   => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    'visible'           => true,
+    'required'          => false,
+    'user_defined'      => true,
+    'default'           => 0
+));
 
 
-/**
- * There are two modes to run this script:
- *
- * 1. Dump available locale options (currencies, locales, timezones) and exit
- * php -f install.php -- --get_options
- *
- * The output can be eval'd in a regular PHP array of the following format:
- * array (
- *   'locale' =>
- *   array (
- *     0 =>
- *     array (
- *       'value' => 'zh_TW',
- *       'label' => 'Chinese (Taiwan)',
- *     ),
- *   ),
- *   'currency' =>
- *   array (
- *     0 =>
- *     array (
- *       'value' => 'zh_TW',
- *       'label' => 'Chinese (Taiwan)',
- *     ),
- *   ),
- *   'timezone' =>
- *   array (
- *     0 =>
- *     array (
- *       'value' => 'zh_TW',
- *       'label' => 'Chinese (Taiwan)',
- *     ),
- *   ),
- * );
- *
- * or parsed in any other way.
- *
- * 2. Perform the installation
- *
- *  php -f install.php -- --license_agreement_accepted yes \
- *  --locale en_US --timezone "America/Los_Angeles" --default_currency USD \
- *  --db_host localhost --db_name magento_database --db_user magento_user --db_pass 123123 \
- *  --db_prefix magento_ \
- *  --url "http://magento.example.com/" --use_rewrites yes \
- *  --use_secure yes --secure_base_url "https://magento.example.com/" --use_secure_admin yes \
- *  --admin_lastname Owner --admin_firstname Store --admin_email "admin@example.com" \
- *  --admin_username admin --admin_password 123123 \
- *  --encryption_key "Encryption Key"
- *
- * Possible options are:
- * --license_agreement_accepted // required, it will accept 'yes' value only
- * Locale settings:
- * --locale                     // required, Locale
- * --timezone                   // required, Time Zone
- * --default_currency           // required, Default Currency
- * Database connection options:
- * --db_host                    // required, You can specify server port, ex.: localhost:3307
- *                              // If you are not using default UNIX socket, you can specify it
- *                              // here instead of host, ex.: /var/run/mysqld/mysqld.sock
- * --db_model                   // Database type (mysql4 by default)
- * --db_name                    // required, Database Name
- * --db_user                    // required, Database User Name
- * --db_pass                    // required, Database User Password
- * --db_prefix                  // optional, Database Tables Prefix
- *                              // No table prefix will be used if not specified
- * Session options:
- * --session_save <files|db>    // optional, where to store session data - in db or files. files by default
- * Web access options:
- * --admin_frontname <path>     // optional, admin panel path, "admin" by default
- * --url                        // required, URL the store is supposed to be available at
- * --skip_url_validation        // optional, skip validating base url during installation or not. No by default
- * --use_rewrites               // optional, Use Web Server (Apache) Rewrites,
- *                              // You could enable this option to use web server rewrites functionality for improved SEO
- *                              // Please make sure that mod_rewrite is enabled in Apache configuration
- * --use_secure                 // optional, Use Secure URLs (SSL)
- *                              // Enable this option only if you have SSL available.
- * --secure_base_url            // optional, Secure Base URL
- *                              // Provide a complete base URL for SSL connection.
- *                              // For example: https://www.mydomain.com/magento/
- * --use_secure_admin           // optional, Run admin interface with SSL
- * Backend interface options:
- * --enable_charts              // optional, Enables Charts on the backend's dashboard
- * Admin user personal information:
- * --admin_lastname             // required, admin user last name
- * --admin_firstname            // required, admin user first name
- * --admin_email                // required, admin user email
- * Admin user login information:
- * --admin_username             // required, admin user login
- * --admin_password             // required, admin user password
- * Encryption key:
- * --encryption_key             // optional, will be automatically generated and displayed on success, if not specified
- *
- */
+$installer->addAttributeToGroup(
+    $entityTypeId,
+    $attributeSetId,
+    $attributeGroupId,
+    'unique_id',
+    '11'                    //позиция атрибута в закладке. В закладке General всего 10 элементов
+);
 
-if (version_compare(phpversion(), '5.2.0', '<')===true) {
-    die('ERROR: Whoops, it looks like you have an invalid PHP version. Magento supports PHP 5.2.0 or newer.');
-}
-set_include_path(dirname(__FILE__) . PATH_SEPARATOR . get_include_path());
-require 'app/Mage.php';
+$attributeId = $installer->getAttributeId($entityTypeId, 'unique_id');
 
-try {
-    $app = Mage::app('default');
+//тут надо обратить внимание на тип атрибута, от этого зависит какую таблицу значений используем (в нашем случае catalog_category_entity_text)
 
-    $installer = Mage::getSingleton('install/installer_console');
-    /* @var $installer Mage_Install_Model_Installer_Console */
+$installer->run("
+INSERT INTO `{$installer->getTable('catalog_category_entity_text')}`
+(`entity_type_id`, `attribute_id`, `entity_id`, `value`)
+    SELECT '{$entityTypeId}', '{$attributeId}', `entity_id`, '1'
+        FROM `{$installer->getTable('catalog_category_entity')}`;
+");
 
-    if ($installer->init($app)          // initialize installer
-        && $installer->checkConsole()   // check if the script is run in shell, otherwise redirect to web-installer
-        && $installer->setArgs()        // set and validate script arguments
-        && $installer->install())       // do install
-    {
-        echo 'SUCCESS: ' . $installer->getEncryptionKey() . "\n";
-        exit;
-    }
+//данная запись установить ваш атрибут для рутовой категории
+Mage::getModel('catalog/category')
+    ->load(1)
+    ->setImportedCatId(0)
+    ->setInitialSetupFlag(true)
+    ->save();
 
-} catch (Exception $e) {
-    Mage::printException($e);
-}
+//данная запись пропишет ваш атрибут в дефолтной категории
+Mage::getModel('catalog/category')
+    ->load(2)
+    ->setImportedCatId(0)
+    ->setInitialSetupFlag(true)
+    ->save();
 
-// print all errors if there were any
-if ($installer instanceof Mage_Install_Model_Installer_Console) {
-    if ($installer->getErrors()) {
-        echo "\nFAILED\n";
-        foreach ($installer->getErrors() as $error) {
-            echo $error . "\n";
-        }
-    }
-}
-exit(1); // don't delete this as this should notify about failed installation
+$installer->endSetup();
+
+
